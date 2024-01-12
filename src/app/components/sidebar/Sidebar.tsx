b7e2b51cd8db2997cb7faddd091fe8e72691dd4c
amyusif <am.yusif530@gmail.com>
2024-01-12 20:34:14 +0000

    added some code

--- src/app/components/sidebar/Sidebar.tsx
+++ src/app/components/sidebar/Sidebar.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
+"use client"
+// components/Sidebar.js
+import React, { useState } from 'react';
+import styles from './sidebar.module.css';
 
 const Sidebar = () => {
-  return (
-    <div>
-      <h1>hello</h1>
-    </div>
-  )
-}
+  const [isExpanded, setExpanded] = useState(true);
 
-export default Sidebar
+  const toggleSidebar = () => {
+    setExpanded(!isExpanded);
+  };
+
+  return (
+    <div className={`${styles.sidebar} ${isExpanded ? styles.expanded : styles.collapsed}`}>
+      <div className={styles.header}>
+        <div className={styles.logo}>Logo</div>
+        <div className={styles.toggleBtn} onClick={toggleSidebar}>
+          ☰
+        </div>
+      </div>
+      <div className={styles.menu}>
+        <div className={styles.tab}>
+          <i className={styles.icon}>Icon1</i>
+          <span className={styles.tabName}>Tab 1</span>
+        </div>
+        <div className={styles.tab}>
+          <i className={styles.icon}>Icon2</i>
+          <span className={styles.tabName}>Tab 2</span>
+        </div>
+        {/* Add more tabs as needed */}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
